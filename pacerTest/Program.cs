f06732d3ece640356f0454e0b4de86d5e3ed5c1c
Kol4mo <47543776+kol4mo@users.noreply.github.com>
2024-03-26 14:03:15 -0600

    Add project files.

---
+++ pacerTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace pacerTest {
+    internal class Program {
+        static void performPacerTest() {
+        }
+
+        static void run() {
+        }
+
+        static void changeDirection() {
+        }
+
+        static int addOne(int addedNumber) {
+            return ++addedNumber;
+        }
+
+        static void Main(string[] args) {
+            int addedNumber = addOne(1);
+            Console.WriteLine(addedNumber);
+        }
+    }
+}
